78ad79e48a51e8ba6fba7b2d96d1714b6b37d80c
Bill Searle <billsearle@gmail.com>
2020-12-22 21:20:19 +0000

    Add grid movement logic

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,117 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
+
+/**
+ * Grid index allowed x or y: 0-4
+ * Bottom Left is 0,0
+ * If x is 0, West is not allowed
+ * If x is 4, East is not allowed
+ * If y is 0, South is not allowed
+ * If y is 4, North is not allowed
+ * If North increment y
+ * If South decrement y
+ * If East increment x
+ * If West decrement x
+ */
 
 export default function Home() {
+  const [botPos, setBotPos] = useState({ x: 0, y: 0, facing: 0 });
+  const [moveError, setMoveError] = useState(false);
+
+  const DIRECTIONS = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+
+  const moveNorth = () => {
+    if (botPos.y === 4) {
+      setMoveError(true);
+    } else {
+      setBotPos({
+        ...botPos,
+        y: botPos.y + 1,
+      });
+    }
+  };
+
+  const moveSouth = () => {
+    if (botPos.y === 0) {
+      setMoveError(true);
+    } else {
+      setBotPos({
+        ...botPos,
+        y: botPos.y - 1,
+      });
+    }
+  };
+
+  const moveEast = () => {
+    if (botPos.x === 4) {
+      setMoveError(true);
+    } else {
+      setBotPos({
+        ...botPos,
+        x: botPos.x + 1,
+      });
+    }
+  };
+
+  const moveWest = () => {
+    if (botPos.x === 0) {
+      setMoveError(true);
+    } else {
+      setBotPos({
+        ...botPos,
+        x: botPos.x - 1,
+      });
+    }
+  };
+
+  const handleMove = () => {
+    switch (botPos.facing) {
+      case 0:
+        moveNorth();
+        break;
+      case 1:
+        moveEast();
+        break;
+      case 2:
+        moveSouth();
+        break;
+      case 3:
+        moveWest();
+        break;
+      default:
+        console.log('Unknown direction: ', DIRECTIONS[botPos.facing]);
+        break;
+    }
+  };
+
+  const handleRotate = (turn) => {
+    let newHeading;
+
+    // turn left, decrements through the directions array
+    if (turn === 'left') {
+      if (botPos.facing === 0) {
+        newHeading = 3;
+      } else {
+        newHeading = botPos.facing - 1;
+      }
+    }
+
+    // turn right, increments though the directions array
+    if (turn === 'right') {
+      if (botPos.facing === 3) {
+        newHeading = 0;
+      } else {
+        newHeading = botPos.facing + 1;
+      }
+    }
+
+    setBotPos({
+      ...botPos,
+      facing: newHeading,
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,55 +121,18 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Table Top Robot
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <section />
+        <pre>{`${botPos.x}, ${botPos.y}, ${DIRECTIONS[botPos.facing]}`}</pre>
+        <pre>{`${moveError}`}</pre>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button onClick={handleMove}>move</button>
+        <button onClick={() => handleRotate('left')}>left</button>
+        <button onClick={() => handleRotate('right')}>right</button>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
